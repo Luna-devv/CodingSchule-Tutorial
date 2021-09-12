03142e391bb773a980e1026affa0c45a7d545929
Luna-devv <luna@xyna.space>
2021-09-12 17:54:45 +0200

    Add better explination

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-// Defined my shit.
+// defined dependencies and other things
+//  remember that this should always be in the first lines of your code
 const { Client, Intents, MessageEmbed, Message } = require("discord.js");
 const client = new Client({
 	intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILD_MESSAGE_REACTIONS, Intents.FLAGS.GUILD_VOICE_STATES, Intents.FLAGS.GUILD_WEBHOOKS, Intents.FLAGS.GUILD_MEMBERS]
@@ -6,33 +7,38 @@ const client = new Client({
 const chalk = require(`chalk`)
 
 
-//my very complicated config system
+// config system | if you add a value in "./config.json" you can use it in the client
+//    here is an example:
+//    If you add "token": "xxx" you can simply use client.token to get your token
 const config = require(`./config.json`);
 Object.keys(config).forEach(async (key) => {
     client[key] = config[key];
-});//this just does when you add *for example* ' "smth": "smth" ' that it defines it in client.
+});
 
 
-//event - on bot start
+// event - on bot start
+//  everything that is in there is triggered once when the bot starts
 client.once(`ready`, () => {
-    console.log(chalk.green(`${client.user.username} is online lmao`));
-    client.user.setPresence({
-        activity: {
-            name: `at Lunas tutorials`,
-            type: `WATCHING`
-        }
-    });
+    console.log(chalk.green(`[client]`), chalk.greenBright(`logged in as ${client.user.tag}.`));
+    client.user.setPresence({ activities: [{ name: 'at Luna <3', type: `WATCHING` }] });
 });
 
 
-//event - on message create
+// event - on message create
+//  everything that is in there triggers when someone writes a message in a channel the bot has access to
 client.on(`messageCreate`, async message => {
+
     // replies when you @mention your bot
-    if(message.content === `<@${client.user.id}>` || message.content === `<@!${client.user.id}>`) return message.channel.send({
-        content: `Hey <@!${message.author.id}>, how are you today?`
-    });
+    if(message.content === `<@${client.user.id}>` || message.content === `<@!${client.user.id}>`){
+        message.channel.send({
+            content: `Hey <@!${message.author.id}>, how are you today?`
+        });
+        return;
+    };
+
 });
 
 
-// Login the client and connect with discord.
+// login the client and connect with discord
+//  remember that this should always be in the last line of your code
 client.login(client.token);
